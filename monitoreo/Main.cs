2498587f02b8ce502eb3ed0a95b579f171d468a7
JohannClever <johann_bldr@hotmail.com>
2013-02-12 16:21:26 -0500

    Monitoreo V 1

---
+++ monitoreo/Main.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Threading;
+
+namespace monitoreo
+{
+	class MainClass
+	{
+		static bool _continue = true;
+		public static void Main (string[] args)
+		{
+			FileSystemWatcher watcher = new FileSystemWatcher();
+			watcher.Path = @"C:\Users\D2\Desktop\origen\";
+			watcher.Filter = "*.txt";
+			watcher.IncludeSubdirectories = false;
+			watcher.Created += new FileSystemEventHandler(OnCreated);
+			watcher.EnableRaisingEvents = true;
+	
+			while (_continue)		
+			{
+				Console.WriteLine("Monitoreando... ");
+				Thread.Sleep(1000);	
+			}
+			
+			
+			
+			Console.ReadKey(true);
+		}
+
+		static void OnCreated(object sender, FileSystemEventArgs e)		
+		{
+			Console.WriteLine("Se ha creado el archivo '{0}'", e.Name);
+			_continue = false;	
+		}
+	}
+}
+
+
+
+
+	
+
+	
